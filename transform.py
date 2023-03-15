731b159f2459ef5fd621d7f15506d5a1439921b6
shoguevara <shoguevara@gmail.com>
2023-03-15 23:17:43 +0300

    first commit

---
+++ transform.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import json
+from cherrypicker import CherryPicker
+
+with open('instaces.json', encoding='utf-8') as f:
+   data = json.loads(f.read())
+jsonBody = pd.json_normalize(data).rename(columns={'id': 'resource_id'})
+df1 = pd.read_csv('20230213.csv')
+merged_data = df1.merge(jsonBody,on=["resource_id"],how="left")
+merged_data.to_csv('merged.csv')
