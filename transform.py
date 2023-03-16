e8241b328a3435f29f19c284a226083e0117d623
shoguevara <shoguevara@gmail.com>
2023-03-16 10:56:02 +0300

    edited requirements.txt/usr/bin/python3

--- transform.py
+++ transform.py
@@ -5,6 +5,9 @@ from cherrypicker import CherryPicker
 with open('instaces.json', encoding='utf-8') as f:
    data = json.loads(f.read())
 jsonBody = pd.json_normalize(data).rename(columns={'id': 'resource_id'})
+picker = CherryPicker(jsonBody)
+jsonpicked = picker['resource_id', 'city*'].get()
+print (picker)
 df1 = pd.read_csv('20230213.csv')
 merged_data = df1.merge(jsonBody,on=["resource_id"],how="left")
 merged_data.to_csv('merged.csv')
