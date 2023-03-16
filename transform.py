b6e8071c48e7c744d42aa1d8274b70dc0515975d
shoguevara <shoguevara@gmail.com>
2023-03-16 13:00:46 +0300

    modified:   transform.py  111

--- transform.py
+++ transform.py
@@ -10,5 +10,5 @@ labels_columns.append("resource_id")
 jsonBody_subset = jsonBody[labels_columns]
 df1 = pd.read_csv('20230213.csv')
 merged_data = df1.merge(jsonBody_subset,on=["resource_id"],how="left")
-#merged_data.to_csv('merged.csv')   
+merged_data.to_csv('merged.csv')   
 print(merged_data)
